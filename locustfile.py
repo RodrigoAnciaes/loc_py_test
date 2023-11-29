21531ff2a5da0088d8a8b93eadff7b7ab56d2c05
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2023-11-29 17:47:25 -0300

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,10 +1,16 @@
 from locust import HttpUser, task, between
 import os
+from dotenv import load_dotenv
+
+# Open the .env file
+load_dotenv(override=True)
 
 # Retrieve the endpoint from the environment variable and ensure it includes the scheme
 API_ENDPOINT = os.getenv("API_ENDPOINT")
 if not API_ENDPOINT.startswith("http"):
     API_ENDPOINT = f"http://{API_ENDPOINT}"
+else:
+    API_ENDPOINT = f"{API_ENDPOINT}"
 
 class ApiUser(HttpUser):
     # The endpoint is set in the environment variable, including the scheme
