256eacbe13152d175349bce02518a917a860e1fe
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2023-12-01 16:33:57 -0300

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -17,27 +17,6 @@ class ApiUser(HttpUser):
     host = API_ENDPOINT
     wait_time = between(1, 5)  # Simulate wait time between tasks
 
-    # @task(2)
-    # def create_user(self):
-    #     # Define a sample user data
-    #     user_data = {"name": "test_user", "email": "test@example.com"}
-    #     self.client.post("/users/", json=user_data)
-
     @task(1)
     def get_users(self):
-        self.client.get("/membros")
-
-    # @task(1)
-    # def update_user(self):
-    #     # Assuming you have some users, you would update one here
-    #     # For simplicity, using a fixed user_id
-    #     user_id = 1
-    #     updated_user_data = {"name": "updated_user", "email": "updated@example.com"}
-    #     self.client.put(f"/users/{user_id}", json=updated_user_data)
-
-    # @task(1)
-    # def delete_user(self):
-    #     # Again, assuming you have users to delete
-    #     # Using a fixed user_id for simplicity
-    #     user_id = 1
-    #     self.client.delete(f"/users/{user_id}")
+        self.client.get("/")
